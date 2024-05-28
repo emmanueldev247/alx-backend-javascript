3d9e47d4a5bef749bff63a164a16c35ad22a6447
emmanueldev247 <mailemmydee@gmail.com>
2024-05-28 04:42:05 +0000

    fixed: stray texts

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,7 +1,7 @@
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-2-arrow.js  const self = this;
+  const self = this;
   this.addNeighborhood = (newNeighborhood) => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;
