fb4fba2f9a443bcda19676495c13ed1b64539fd4
emmanueldev247 <mailemmydee@gmail.com>
2024-06-01 00:26:56 +0000

    added: Iterator

---
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -0,0 +1,6 @@
+// Iterator - function named createEmployeesObject that will receive two arguments
+export default function createEmployeesObject(departmentName, employees) {
+  return {
+    [departmentName]: employees,
+  };
+}
