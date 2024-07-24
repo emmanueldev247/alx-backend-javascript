4c4782679f49434daaa0759b2414d23ef65e122d
Emmanuel Ademola <mailemmydee@gmail.com>
2024-07-24 15:15:30 +0000

    added: Handle multiple successful promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,8 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((user) => console.log(`${user[0].body} ${user[1].firstName} ${user[1].lastName}`))
+    .catch(() => console.log('Signup system offline'));
+}
