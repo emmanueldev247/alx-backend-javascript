51583a484587094350055e406eb74680fec5544e
Emmanuel Ademola <mailemmydee@gmail.com>
2024-07-24 15:15:06 +0000

    added: Don't make a promise...if you know you can't keep it

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+// Using the prototype, return a promise
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success === true) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
