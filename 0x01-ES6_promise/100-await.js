c5a2d4869d5a23a00814f6132d8d6f02a55d6fb2
Emmanuel Ademola <mailemmydee@gmail.com>
2024-07-26 17:13:00 +0000

    updated: error handling & structure

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,24 +1,15 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const result = {
-    photo: null,
-    user: null,
-  };
-
   try {
-    const photo = await uploadPhoto();
-    result.photo = photo;
+    return {
+      photo: await uploadPhoto(),
+      user: await createUser(),
+    };
   } catch (err) {
-    return result;
+    return {
+      photo: null,
+      user: null,
+    };
   }
-
-  try {
-    const user = await createUser();
-    result.user = user;
-  } catch (err) {
-    return result;
-  }
-
-  return result;
 }
