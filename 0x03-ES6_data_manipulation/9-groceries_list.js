f8877190aac2fdab637237945e85ee11a337462e
Emmanuel Ademola <mailemmydee@gmail.com>
2024-07-30 15:34:03 +0000

    added: Map data structure

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+export default function groceriesList() {
+  const mapA = new Map();
+
+  mapA.set('Apples', 10);
+  mapA.set('Tomatoes', 10);
+  mapA.set('Pasta', 1);
+  mapA.set('Rice', 1);
+  mapA.set('Banana', 5);
+
+  return mapA;
+}
