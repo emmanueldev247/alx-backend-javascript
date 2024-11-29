6e73cd7a3069e32767b3b9329a03aeca3ab852a8
Emmanuel Ademola <mailemmydee@gmail.com>
2024-11-29 17:10:52 +0000

    updated: Rewrite the test suite, using expect from Chai

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,121 +1,121 @@
-const { assert } = require('chai');
+const { expect } = require('chai');
 const calculateNumber = require('./2-calcul_chai');
 
 describe("SUM: Sum of 2 numbers", () => {
   it("checking if number is round for 2 whole numbers", () => {
-    assert.strictEqual(calculateNumber('SUM', 2, 3), 5);
+    expect(calculateNumber('SUM', 2, 3)).to.equal(5);
   });
 
   it("checking if number is round for first float & second whole", () => {
-    assert.strictEqual(calculateNumber('SUM', 1.2, 3), 4);
+    expect(calculateNumber('SUM', 1.2, 3)).to.equal(4);
   });
 
   it("checking if number is round for first float & second whole", () => {
-    assert.strictEqual(calculateNumber('SUM', 1.5, 3), 5);
+    expect(calculateNumber('SUM', 1.5, 3)).to.equal(5);
   });
 
   it("checking if number is round for first whole & second float", () => {
-    assert.strictEqual(calculateNumber('SUM', 3, 1.2), 4);
+    expect(calculateNumber('SUM', 3, 1.2)).to.equal(4);
   });
 
   it("checking if number is round for first whole & second float", () => {
-    assert.strictEqual(calculateNumber('SUM', 3, 1.5), 5);
+    expect(calculateNumber('SUM', 3, 1.5)).to.equal(5);
   });
 
   it("checking if number is round for 2 float numbers", () => {
-    assert.strictEqual(calculateNumber('SUM', 3.1, 1.2), 4);
+    expect(calculateNumber('SUM', 3.1, 1.4)).to.equal(4);
   });
 
   it("checking if number is round for 2 float numbers", () => {
-    assert.strictEqual(calculateNumber('SUM', 3.5, 1.2), 5);
+    expect(calculateNumber('SUM', 3.5, 1.2)).to.equal(5);
   });
 
   it("checking if number is round for 2 float numbers", () => {
-    assert.strictEqual(calculateNumber('SUM', 3.1, 1.5), 5);
+    expect(calculateNumber('SUM', 3.1, 1.5)).to.equal(5);
   });
 
   it("checking if number is round for 2 float numbers", () => {
-    assert.strictEqual(calculateNumber('SUM', 3.6, 1.8), 6);
+    expect(calculateNumber('SUM', 3.6, 1.8)).to.equal(6);
   });
 });
 
 describe("SUBTRACT: Subtraction of b from a", () => {
   it("checking if number is round for 2 whole numbers", () => {
-    assert.strictEqual(calculateNumber('SUBTRACT', 8, 3), 5);
+    expect(calculateNumber('SUBTRACT', 8, 3)).to.equal(5);
   });
 
   it("checking if number is round for first float & second whole", () => {
-    assert.strictEqual(calculateNumber('SUBTRACT', 7.2, 3), 4);
+    expect(calculateNumber('SUBTRACT', 7.2, 3)).to.equal(4);
   });
 
   it("checking if number is round for first float & second whole", () => {
-    assert.strictEqual(calculateNumber('SUBTRACT', 7.5, 3), 5);
+    expect(calculateNumber('SUBTRACT', 7.5, 3)).to.equal(5);
   });
 
   it("checking if number is round for first whole & second float", () => {
-    assert.strictEqual(calculateNumber('SUBTRACT', 5, 1.2), 4);
+    expect(calculateNumber('SUBTRACT', 5, 1.2)).to.equal(4);
   });
 
   it("checking if number is round for first whole & second float", () => {
-    assert.strictEqual(calculateNumber('SUBTRACT', 7, 1.5), 5);
+    expect(calculateNumber('SUBTRACT', 7, 1.5)).to.equal(5);
   });
 
   it("checking if number is round for 2 float numbers", () => {
-    assert.strictEqual(calculateNumber('SUBTRACT', 5.1, 1.2), 4);
+    expect(calculateNumber('SUBTRACT', 5.1, 1.2)).to.equal(4);
   });
 
   it("checking if number is round for 2 float numbers", () => {
-    assert.strictEqual(calculateNumber('SUBTRACT', 5.5, 1.2), 5);
+    expect(calculateNumber('SUBTRACT', 5.5, 1.2)).to.equal(5);
   });
 
   it("checking if number is round for 2 float numbers", () => {
-    assert.strictEqual(calculateNumber('SUBTRACT', 7.1, 1.5), 5);
+    expect(calculateNumber('SUBTRACT', 7.1, 1.5)).to.equal(5);
   });
 
   it("checking if number is round for 2 float numbers", () => {
-    assert.strictEqual(calculateNumber('SUBTRACT', 7.6, 1.8), 6);
+    expect(calculateNumber('SUBTRACT', 7.6, 1.8)).to.equal(6);
   });
 });
 
 describe("DIVIDE: Division of 2 numbers", () => {
   it("checking if number is round for 2 whole numbers", () => {
-    assert.strictEqual(Math.round(calculateNumber('DIVIDE', 8, 3)), 3);
+    expect(Math.round(calculateNumber('DIVIDE', 8, 3))).to.equal(3);
   });
 
   it("checking if number is round for first float & second whole", () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 12.2, 3), 4.0);
+    expect(calculateNumber('DIVIDE', 12.2, 3)).to.equal(4.0);
   });
 
   it("checking if number is round for first float & second whole", () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 14.5, 3), 5.0);
+    expect(calculateNumber('DIVIDE', 14.5, 3)).to.equal(5.0);
   });
 
   it("checking if number is round for first whole & second float", () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 20, 5.2), 4.0);
+    expect(calculateNumber('DIVIDE', 20, 5.2)).to.equal(4.0);
   });
 
   it("checking if number is round for first whole & second float", () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 10, 1.5), 5.0);
+    expect(calculateNumber('DIVIDE', 10, 1.5)).to.equal(5.0);
   });
 
   it("checking if number is round for 2 float numbers", () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 2.1, 1.2), 2.0);
+    expect(calculateNumber('DIVIDE', 2.1 ,1.2)).to.equal(2.0);
   });
 
   it("checking if number is round for 2 float numbers", () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 4.5, 1.2), 5.0);
+    expect(calculateNumber('DIVIDE', 4.5, 1.2)).to.equal(5.0);
   });
 
   it("checking if number is round for 2 float numbers", () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 7.1, 1.5), 3.5);
+    expect(calculateNumber('DIVIDE', 7.1, 1.5)).to.equal(3.5);
   });
 
   it("checking if number is round for 2 float numbers", () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 7.6, 1.8), 4.0);
+    expect(calculateNumber('DIVIDE', 7.6, 1.8)).to.equal(4.0);
   });
 
   it("checking if 'Error' is returned", () => {
-    assert.strictEqual(calculateNumber('DIVIDE', 7.6, 0.1), "Error");
+    expect(calculateNumber('DIVIDE', 7.6, 0.1)).to.equal("Error");
   });
 });
 
