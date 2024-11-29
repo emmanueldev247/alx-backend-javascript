6b799c2aef760a94537079dd0d1f925189366c60
Emmanuel Ademola <mailemmydee@gmail.com>
2024-11-29 17:30:15 +0000

    updated: arrow function

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,11 +1,12 @@
 const Utils = {
-  calculateNumber: (type, a, b) {
-  if (type === "SUM") return Math.round(a) + Math.round(b);
-  if (type === "SUBTRACT") return Math.round(a) - Math.round(b);
-  if (type === "DIVIDE") {
-    const roundA = Math.round(a);
-    const roundB = Math.round(b);
-    return roundB === 0 ? "Error" : roundA / roundB;
+  calculateNumber: (type, a, b) => {
+    if (type === "SUM") return Math.round(a) + Math.round(b);
+    if (type === "SUBTRACT") return Math.round(a) - Math.round(b);
+    if (type === "DIVIDE") {
+      const roundA = Math.round(a);
+      const roundB = Math.round(b);
+      return roundB === 0 ? "Error" : roundA / roundB;
+    }
   }
 };
 
