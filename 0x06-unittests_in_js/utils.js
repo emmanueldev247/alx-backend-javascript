d54bd2136c6083a11dee7fa4e34c464884a6369a
Emmanuel Ademola <mailemmydee@gmail.com>
2024-11-29 17:17:38 +0000

    added: utils

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,13 @@
+const Utils = {
+  calculateNumber: (type, a, b) {
+  if (type === "SUM") return Math.round(a) + Math.round(b);
+  if (type === "SUBTRACT") return Math.round(a) - Math.round(b);
+  if (type === "DIVIDE") {
+    const roundA = Math.round(a);
+    const roundB = Math.round(b);
+    return roundB === 0 ? "Error" : roundA / roundB;
+  }
+};
+
+
+module.exports = Utils;
