da14d4c11af65fe621795b037f6764635fcf1f15
Emmanuel Ademola <mailemmydee@gmail.com>
2024-11-30 16:01:17 +0000

    removed: '\n' from the response end

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,7 +4,7 @@ const port = 1245;
 
 const app = createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Holberton School!\n');
+  res.end('Hello Holberton School!');
 });
 
 app.listen(port);
