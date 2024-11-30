a1c2f8cddde7405b201a898bf5e6e4fc2f5b77f1
Emmanuel Ademola <mailemmydee@gmail.com>
2024-11-30 14:47:26 +0000

    added: Reading a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf-8');
+
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    lines.shift();
+
+    let totalStudents = 0;
+    const fieldGroup = {};
+
+    for (const line of lines) {
+      const [firstname, lastname, age, field] = line.split(',');
+
+      if (firstname && lastname && age && field) {
+        if (!fieldGroup[field]) fieldGroup[field] = [];
+        fieldGroup[field].push(firstname);
+
+        totalStudents += 1;
+      }
+    }
+
+    console.log(`Number of students: ${totalStudents}`);
+
+    for (const [field, names] of Object.entries(fieldGroup)) {
+      console.log(
+        `Number of students in ${field}: ${names.length}. ${names.join(', ')}`,
+      );
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
